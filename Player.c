b6c97fc3df41f73010a96f65fbc387577a1b9140
LouisAML <amelineau.louis78@gmail.com>
2017-10-04 17:02:41 +0200

    Dynamic allocation for tabPlayer.

--- Player.c
+++ Player.c
@@ -7,7 +7,18 @@ void initPlayer (Player* pointeur)
     pointeur->pos_x=20;
     pointeur->pos_y=21;
 }
+int alloctabPlayer (Player*pointeur, int dim)
+{
+    pointeur = null;
 
-Player tabPlayer [10];
+    pointeur = malloc(dim*sizeof(player));
+    if (pointeur == null) // si l'allocation a échoué
+    {
+        exit(0); // on arrête immédiatement le programme
+    }
 
+    // on peut continuer le programme normalement sinon
+
+    return (pointeur);
+}
 
