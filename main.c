92fed3c39bdce2af80ad606499fa44561191cf43
LouisAML <amelineau.louis78@gmail.com>
2017-10-05 11:46:12 +0200

    Menu fonction implementation.

--- main.c
+++ main.c
@@ -16,27 +16,93 @@ int main()
 
     Player *tabPlayer = dynamicAllocPlayer(dim);
     affichageTabPlayer(tabPlayer, dim);
-printf("\n\n");
+    printf("\n\n");
     Box *tabBox = dynamicAllocBox(dim);
     affichageTabBox(tabBox, dim);
-printf("\n\n");
+    printf("\n\n");
     Wall *tabWall = dynamicAllocWall(dim);
     affichageTabWall(tabWall, dim);
-menu();
+    menu();
 
     return 0;
 }
 int menu ()
 {
+    int choix=0;
+
+    while (choix !=100)
+    {
     system("cls");
-    printf("##########################################################################\n");
-    printf("##########################################################################\n");
-    printf("##########################################################################\n");
-    printf("#############                 SOKOBAN                     ################\n");
-    printf("##########################################################################\n");
-    printf("##########################################################################\n");
-    printf("##########################################################################\n\n");
-    printf("                                1. Jouer");
-    printf("                                2. P");
+    printf("                                                                          \n");
+    printf("                                                                          \n");
+    printf("                                                                          \n");
+    printf("                          -       SOKOBAN       -                         \n");
+    printf("                                                                          \n");
+    printf("                                                                          \n");
+    printf("                                                                        \n\n");
+    printf("                                1. Jouer     \n\n");
+    printf("                                2. Option    \n\n");
+    printf("                                3. Credit    \n\n");
+    printf("                                4. Quitter    \n\n");
+
+        scanf("%d",&choix);
+        switch (choix)
+        {
+        case 1 : // jouer
+            break;
+        case 2 :  choix = option();
+            break;
+        case 3 : //  Credit
+            break;
+        case 4 :
+            exit(0);// quitter
+            break;
+        default:
+            choix =0;
+            break;
+        }
+    }
     return (0);
 }
+
+int option ()
+{
+    int choix=0;
+
+    while (choix !=101)
+    {
+
+    system("cls");
+    printf("                                                                          \n");
+    printf("                                                                          \n");
+    printf("                                                                          \n");
+    printf("                          -       SOKOBAN       -                         \n");
+    printf("                                                                          \n");
+    printf("                                                                          \n");
+    printf("                                                                        \n\n");
+    printf("                                1. Affichage console     \n\n");
+    printf("                                2. Affichage Allegro    \n\n");
+    printf("                                3. Musique On/Off    \n\n");
+    printf("                                4. Retour    \n\n");
+
+        scanf("%d",&choix);
+        switch (choix)
+        {
+        case 1 : // affichage console
+            break;
+        case 2 :  // affichage allegro
+            break;
+        case 3 : //  musique
+            break;
+        case 4 :
+            choix = 101; // retour
+            break;
+        default:
+            choix = 0;
+            break;
+        }
+    }
+
+    return(choix);
+}
+
