96f8ad2ae9bd76512f4b254e065b619d401766ba
darkichigo13 <danisanchezpalma@gmail.com>
2017-10-12 14:41:06 +0200

    Add function 'movePlayer' and init function 'verifyMove'.

--- Player.c
+++ Player.c
@@ -1,5 +1,6 @@
 #include "Player.h"
 #include <stdlib.h>
+#include <stdbool.h>
 
 Player * dynamicAllocPlayer (int dim)
 {
@@ -31,4 +32,50 @@ void affichageTabPlayer (Player * tab, int dim)
     {
     printf("Player posx : %d / Player posy : %d\n",tab[i].pos_x,tab[i].pos_y);
     }
+}
+
+void movePlayer (Player *player, char deplacement){
+    bool response;
+    switch (deplacement){
+    case 'z' :
+        response = verifyMove(player->pos_x , (player->pos_y - 1));
+        if (response){
+            player->pos_y = player->pos_y - 1;
+        }
+        else if (!response){
+            //TO-DO return error
+        }
+
+    case 'q' :
+        response = verifyMove((player->pos_x - 1), player->pos_y);
+        if (response){
+            player->pos_x = player->pos_x - 1;
+        }
+        else if (!response){
+            //TO-DO return error
+        }
+
+    case 's' :
+        response = verifyMove(player->pos_x, (player->pos_y + 1));
+        if (response){
+            player->pos_y = player->pos_y + 1;
+        }
+        else if (!response){
+            //TO-DO return error
+        }
+
+    case 'd' :
+        response = verifyMove((player->pos_x + 1), player->pos_y);
+        if (response){
+            player->pos_x = player->pos_x + 1;
+        }
+        else if (!response){
+            //TO-DO return error
+        }
+    }
+}
+
+bool verifyMove (int pos_x, int pos_y){
+    //TO-DO
+    return (1);
 }
